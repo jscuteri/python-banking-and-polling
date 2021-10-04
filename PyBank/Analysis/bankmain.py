7c94351657b9f845e43c55973f232d2e5a6e30a9
Joe Scuteri <joescuteri@Joe-Macbook-Air.local>
2021-10-04 17:39:09 -0400

    committing bank file October 4th at 5:40 PM

---
+++ PyBank/Analysis/bankmain.py
@@ -0,0 +1,18 @@
+#import module for file path
+import os
+
+# import module for reading csv files
+import csv
+
+#find the file path for the csv file
+csvbankpath = os.path.join('Resources', 'budget_data.csv')
+
+#open the csv file
+#with open(csvbankpath) as csvbankfile:
+
+    # CSV reader specifies delimiter and variable that holds contents
+  #  csvbankreader = csv.reader(csvbankfile, delimiter=',')
+
+   # print(csvbankreader)
+ 
+    
